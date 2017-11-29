206d5a0796f33507761aa7683719846dae0736c7
Blagojce Jovanoski <blagojcejovanoski@gmail.com>
2017-11-29 07:27:21 +0100

    Added vehicle-form component.

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,16 +1,22 @@
+import { RouterModule } from '@angular/router';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { VehicleFormComponent } from './components/vehicle-form/vehicle-form.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    VehicleFormComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      {path:'vehicles/new', component:VehicleFormComponent}
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
